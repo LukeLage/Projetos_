f807041f012a432fcb4823750ab93a5f82a24b2d
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-01-19 21:04:15 -0300

    Add files via upload

---
+++ Sistema de Notas.py
@@ -0,0 +1,137 @@
+quantidade_alunos = int(input("Insira quantos alunos há na classe: \n"))
+quantidade_provas = int(input("Insira quantos trabalhos avaliativos houveram no semestre: \n"))
+nota_max = int(input('Insira a nota máxima do semestre: \n'))
+
+lista_alunos = []
+
+# Função que fará o trabalho de compilar os nomes, matriculas e notas dos alunos
+def notas():
+    nome = input("Insira o nome do aluno: \n")
+    matricula = input("Insira a matrícula do aluno: \n")
+    lista_alunos = [{"Matricula": matricula}, {"Nome": nome}]
+    soma_notas = 0
+
+    for i in range(quantidade_provas):
+        nota = float(input("Insira a nota do aluno do trabalho avaliativo de número: {} \n".format(i + 1)))
+        peso = float(input("Insira o peso do trabalho avaliativo (de 10 a 0) de número: {} \n".format(i + 1)))
+        valor = peso / 10
+        resultado = nota * valor
+        soma_notas += resultado
+        lista_alunos.append([{"Trabalho avaliativo {}".format(i + 1): resultado}])
+    lista_alunos.append({"Soma das Notas": soma_notas})
+    return lista_alunos
+
+
+for _ in range(quantidade_alunos):
+    lista_alunos.append(notas())
+
+#Função para selecionar um aluno em especifico para verificar sua nota total
+def nota_aluno(matricula_aluno):
+    for aluno in lista_alunos:
+        for item in aluno:
+            if 'Matricula' in item and item ['Matricula'] == matricula_aluno:
+                for nota in aluno:
+                    if 'Soma das Notas' in nota:
+                        return nota['Soma das Notas']
+        return 'Aluno não encontrado'
+
+    matricula_aluno = input('Insira a matrícula do aluno que você deseja verificar a nota: \n')
+    print(nota_aluno(matricula_aluno))
+
+#Função que avalia se as notas dos alunos são válidas considerando a nota máxima possível do semestre
+for aluno in lista_alunos:
+    soma_notas = 0
+    matricula = ''
+    nome = ''
+    for item in aluno: 
+        if 'Soma das Notas' in item:
+            soma_notas = item['Soma das Notas']
+        if 'Matricula' in item:
+            matricula = item['Matricula']
+        if 'Nome' in item: 
+            nome = item['Nome']
+    if soma_notas > nota_max:
+        print('O seguinte aluno está com a nota total maior do que a nota máxima informada para o sistema: ')
+        print('Matrícula: {}, nome: {}, com a nota de: {}' .format(matricula, nome, soma_notas))
+
+#Função para verificar a média geral da turma
+def media_geral():
+    soma_total = 0
+    for aluno in lista_alunos:
+        for item in aluno:
+            if 'Soma das Notas' in item:
+                soma_total += item['Soma das Notas']
+                media = soma_total / quantidade_alunos
+                print('A média geral da turma é de: {:.2f}' .format(media))
+
+#Função para verificar a média de um aluno
+def media_aluno ():
+    for aluno in lista_alunos:
+        for item in aluno:
+            if 'Matricula' in item and item ['Matricula'] == matricula_aluno:
+                soma_notas = 0
+                quantidade_notas = 0
+                for nota in aluno:
+                    if 'Trabalho avaliativo' in nota:
+                        soma_notas += nota ['Trabalho avaliativo']
+                        quantidade_notas += 1
+                        if quantidade_notas > 0:
+                            media = soma_notas / quantidade_notas
+                            return media
+        return 'Aluno não encontrado'
+    
+    matricula_aluno = input('Digite a matrícula do aluno que deseja verificar a nota: \n')
+    print(media_aluno(matricula_aluno))
+    
+#Função para verificar todas as notas de um aluno especifico
+def notas_aluno(matricula_aluno):
+    for aluno in lista_alunos:
+        for item in aluno:
+            if 'Matricula' in item and item ['Matricula'] == matricula_aluno:
+                notas = []
+                for nota in aluno:
+                    if 'Trabalho avaliativo' in nota:
+                        notas.append(nota['Trabalho avaliativo'])
+                        return notas
+        return 'Aluno não encontrado'
+    
+    matricula_aluno = input('Insira a matrícula do aluno que você deseja verificar as notas: \n')
+    print(notas_aluno(matricula_aluno))
+
+#Função para verificação de notas
+verificacao = input('Deseja verificar alguma nota? \n [Sim/Nao] \n')
+verificacao = verificacao.upper()
+
+if verificacao == 'SIM':
+    opcoes = [
+    'Digite a opção desejada:', 
+    '1 - Verificar nota total de aluno específico', 
+    '2 - Verificar todas as notas de um aluno',
+    '3 - Verificar todas as notas da sala', 
+    '4 - Verificar a média geral da sala',
+    '5 - Verificar média de aluno especifico'
+    ]
+    print(opcoes)
+    while True:
+        selecao = int(input('Digite a opção que deseja verificar: \n'))
+        if selecao == 1:
+            nota_aluno()
+            break
+        elif selecao == 2: 
+            notas_aluno()
+            break
+        elif selecao == 3: 
+            print(lista_alunos)
+            break
+        elif selecao == 4:
+            media_geral()
+            break
+        elif selecao == 5:
+            media_aluno()
+            break
+        else:
+            print('Opção inválida!')
+elif verificacao == 'NAO':
+    print('Obrigado!')
+else:
+    print('Opção inválida')
